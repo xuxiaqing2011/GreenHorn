bcfe41820ed06139073d7b6af2ae46c8565f53f9
sbirvin1s <sbirvin1s@gmail.com>
2022-08-13 17:43:57 -0500

    adds materialUI and Styled-Components to repo

---
+++ PostJob.jsx
@@ -0,0 +1,39 @@
+/*========== EXTERNAL MODULES ==========*/
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
+
+
+/*========== INTERNAL MODULES ==========*/
+
+
+
+/*========== EXPORTS ==========*/
+export default function PostJob() {
+
+  /*----- STATE HOOKS -----*/
+    // const [] = useState();
+
+
+  /*----- LIFESTYLE METHODS -----*/
+  // useEffect(() =>  {}, []);
+
+  /*----- EVENT HANDLERS -----*/
+
+
+  /*----- RENDER METHODS -----*/
+
+
+  /*----- RENDERER -----*/
+  return (
+    <>
+      <form>
+
+      </form>
+    </>
+  )
+}
+
+
+
+
+/*========== STYLES ==========*/
