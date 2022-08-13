bc468980619d7dcaeecc10503f42d097bf04b3e9
Xiaqing Xu <xuxiaqing2011@hotmail.com>
2022-08-13 13:54:06 -0500

    added react router

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Route } from "react-router";
 import { createRoot } from "react-dom/client";
 const root = createRoot(document.getElementById("root"));
+
 
 const App = () => {
   return <h1>Hello World</h1>
