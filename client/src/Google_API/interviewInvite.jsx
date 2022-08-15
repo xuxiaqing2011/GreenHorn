a4c754484e6fda06b777210953231ab45457411f
Nicholas Kempkes <nicholas.kempkes@gmail.com>
2022-08-15 17:30:42 -0500

    Fixed geolocation.jsx

---
+++ client/src/Google_API/interviewInvite.jsx
@@ -0,0 +1,79 @@
+import React, { useState, useContext } from 'react';
+import styled from 'styled-components';
+import { Column } from '../../public/stylesheets/styles.js'
+import { AllContext } from "../index.jsx";
+import sendInvite from "./calendar_event.jsx";
+
+import geoConverter from "./geolocation.jsx";
+
+export const InterviewInviteModal = () => {
+  const [show, setShow] = useState(false);
+  const [inviteInfo, setInviteInfo] = useState({});
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleChange = (e) => {
+    setInviteInfo(prevState => ({
+      inviteInfo: {
+        ...prevState.inviteInfo,
+        [e.name]: e.value
+      }
+    }))
+  }
+  const handleSubmit = (e) => {
+    sendInvite(inviteInfo);
+    handleClose();
+  }
+
+  return (
+    <div>
+    <button onClick = {() => handleShow()} >Test</button>
+    <ModalDiv block={ show ? 'block' : 'none' }>
+      <ContentDiv>
+        <form>
+          <label><b>Start Date</b></label>
+          <input name="date" type="date" onChange={(e) => handleChange(e.target)}/>
+          <label><b>Start Time</b></label>
+          <input name="time" type="time" onChange={(e) => handleChange(e.target)}/>
+          <label><b>End Date</b></label>
+          <input name="date" type="date" onChange={(e) => handleChange(e.target)}/>
+          <label><b>End Time</b></label>
+          <input name="time" type="time" onChange={(e) => handleChange(e.target)}/>
+          <label><b>Location</b></label>
+          <input name="location" type="text" placeholder="zoom.url OR 123 adress" onChange={(e) => handleChange(e.target)}/>
+          <label><b>Summary</b></label>
+          <input name="summary" type="text" placeholder="Important information for applicant to know"
+            onChange={(e) => handleChange(e.target)}/>
+        </form>
+        <button onClick= {() => handleSubmit()}>Submit</button>
+        <button onClick = {handleClose}> Close </button>
+      </ContentDiv>
+    </ModalDiv>
+    </div>
+  )
+};
+
+
+const ModalDiv = styled.div`
+  display: ${p => p.block && p.block};
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.6);
+`;
+
+
+const ContentDiv = styled.div`
+  position: fixed;
+  background: white;
+  width: 50%;
+  height: auto;
+  top: 50%;
+  left: 50%;
+  border-radius: 10px;
+  padding: 2rem;
+  transform: translate(-50%, -50%);
+`;
--- client/src/pages/recruiter.js
+++ client/src/pages/recruiter.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import EmbedCalendar from '../Google_API/calendar.jsx'
+import { InterviewInviteModal } from '../Google_API/interviewInvite.jsx'
 
 const Recruiter = () => {
+
   return (
     <>
       <h1>Recruiter View </h1>
