2d570980e79c7c9d05b25df11c417bcfa9ea9176
sbirvin1s <sbirvin1s@gmail.com>
2022-08-16 15:19:52 -0500

    updates data fields to match expected shape of data

--- client/src/components/PostJob/PostJob.jsx
+++ client/src/components/PostJob/PostJob.jsx
@@ -1,11 +1,11 @@
 /*========== EXTERNAL MODULES ==========*/
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Button from '@mui/material/Button';
 import axios from 'axios';
 
 /*========== INTERNAL MODULES ==========*/
-import {Form, Label, Row, Input, ButtonGroup} from '../../../public/stylesheets/styles.js';
+import { Form, Label, Row, Input, ButtonGroup } from '../../../public/stylesheets/styles.js';
 import geoConverter from '../../Google_API/geolocation.jsx'
 
 
@@ -13,7 +13,8 @@ import geoConverter from '../../Google_API/geolocation.jsx'
 export default function PostJob() {
 
   /*----- STATE HOOKS -----*/
-  const [jobPosting, setJobPosting] = useState({});
+  const [jobPosting, setJobPosting] = useState({ recruiter_uuid: 'rxXyEEJqImavbPtUBHrINcvIK5p2' });
+  const [keywords, setKeywords] = useState([]);
   const [zipCode, setZipCode] = useState();
 
 
@@ -21,14 +22,18 @@ export default function PostJob() {
   // useEffect(() =>  {}, []);
 
   /*----- EVENT HANDLERS -----*/
-  const handleChange = ({target: {name, value}}) => {
+  const handleChange = ({ target: { name, value } }) => {
     setJobPosting(prev => ({
       ...prev,
       [name]: value
     }))
   };
 
-  const handleClick = ({target: {name, value}}) => {
+  const handleKeyWords = ({ target: { value } }) => {
+    setKeywords(value)
+  };
+
+  const handleClick = ({ target: { name, value } }) => {
     // set modify the background of the clicked button to be a different color
     setJobPosting(prev => ({
       ...prev,
@@ -39,12 +44,38 @@ export default function PostJob() {
   const handlePostJob = () => {
     event.preventDefault();
     if (jobPosting) {
-      if (jobPosting.salaryStart < (jobPosting.salaryEnd * .8)) {
+      if (jobPosting.salary_low < (jobPosting.salary_high * .8)) {
         alert('Minimum Salary must be within 20% of Maximum Salary')
         return
       }
     }
     // geoConverter(jobPosting.zipcode);
+    /*
+    TODO:
+     - add conversion to Laat and long
+     - attach Lat and Long to state as coord_lat and coord_long
+     - remove zipcode property from state object
+    */
+   if (jobPosting.employment_type && jobPosting.employment_type === 'remote') {
+      setJobPosting(prev => ({
+        ...prev,
+        isRemote: true
+      }))
+    } else {
+     setJobPosting(prev => ({
+       ...prev,
+       isRemote: false
+     }))
+   }
+   if (keywords) {
+      const keywordString = keywords.join(',');
+      setJobPosting(prev => ({
+        ...prev,
+        requested_keywords: keywordString
+      }))
+   }
+
+
     alert('Job Posted');
   };
 
@@ -78,10 +109,10 @@ export default function PostJob() {
         </Input>
       </Label>
         <ButtonGroup>
-          <Button value='inPerson' name='jobLocation' onClick={handleClick}>In Person</Button>
-          <Button value='hybrid' name='jobLocation' onClick={handleClick}>Hybrid</Button>
-          <Button value='remote' name='jobLocation' onClick={handleClick}>Remote</Button>
-          <Button value='onTheRoad' name='jobLocation' onClick={handleClick}>On the Road</Button>
+          <Button value='inPerson' name='job_type' onClick={handleClick}>In Person</Button>
+          <Button value='hybrid' name='job_type' onClick={handleClick}>Hybrid</Button>
+          <Button value='remote' name='job_type' onClick={handleClick}>Remote</Button>
+          <Button value='onTheRoad' name='job_type' onClick={handleClick}>On the Road</Button>
         </ButtonGroup>
       </Label>
     )
@@ -93,7 +124,7 @@ export default function PostJob() {
         <Input
           type='text'
           placeholder='Technology'
-          name='jobIndustry'
+          name='industry'
           onChange={handleChange}
           >
         </Input>
@@ -107,7 +138,7 @@ export default function PostJob() {
         <Input
           type='text'
           placeholder='Mad Scientist'
-          name='jobTitle'
+          name='title'
           onChange={handleChange}
           >
         </Input>
@@ -121,8 +152,8 @@ export default function PostJob() {
       <Label>Salary Start
         <Input
           type='number'
-          placeholder='0'
-          name='salaryStart'
+          placeholder='80000'
+          name='salary_low'
           onChange={handleChange}
           >
         </Input>
@@ -131,7 +162,7 @@ export default function PostJob() {
         <Input
           type='number'
           placeholder='100000'
-          name='salaryEnd'
+          name='salary_high'
           onChange={handleChange}
           >
         </Input>
@@ -144,11 +175,11 @@ const renderPayRate = () => {
   return (
     <Label>Pay Rate
       <ButtonGroup>
-        <Button value='hourly' name='payRate' onClick={handleClick}>per hour</Button>
-        <Button value='daily' name='payRate' onClick={handleClick}>per day</Button>
-        <Button value='weekly' name='payRate' onClick={handleClick}>per week</Button>
-        <Button value='monthly' name='payRate' onClick={handleClick}>per month</Button>
-        <Button value='yearly' name='payRate' onClick={handleClick}>per year</Button>
+        <Button value='hourly' name='pay_adjuster' onClick={handleClick}>per hour</Button>
+        <Button value='daily' name='pay_adjuster' onClick={handleClick}>per day</Button>
+        <Button value='weekly' name='pay_adjuster' onClick={handleClick}>per week</Button>
+        <Button value='monthly' name='pay_adjuster' onClick={handleClick}>per month</Button>
+        <Button value='yearly' name='pay_adjuster' onClick={handleClick}>per year</Button>
       </ButtonGroup>
     </Label>
   )
@@ -160,10 +191,28 @@ const renderDescription = () => {
       <Input
         type='text'
         placeholder='We are an awesome company looking to add someone amazing to our team . . .'
-        name='jobDesc'
+        name='description'
         onChange={handleChange}
         >
       </Input>
+    </Label>
+  )
+}
+
+  const renderKeywords = () => {
+  return (
+    <Label>Requested Keywords
+      <Input
+        type='text'
+        placeholder='chemisty, science, scientist, experiment'
+        name='keywords'
+        onChange={handleKeyWords}
+        >
+      </Input>
+      <p
+        sx={{color:'grey'}}
+        >comma delimited
+      </p>
     </Label>
   )
 }
@@ -174,7 +223,7 @@ const renderAvailablePositions = () => {
       <Input
         type='number'
         placeholder='1'
-        name='availablePositions'
+        name='num_positions'
         onChange={handleChange}
         >
       </Input>
@@ -186,12 +235,12 @@ const renderJobType = () => {
   return (
     <Label>Job Type
       <ButtonGroup>
-        <Button value='fullTime' name='jobType' onClick={handleClick}>Full Time</Button>
-        <Button value='partTime' name='jobType' onClick={handleClick}>Part Time</Button>
-        <Button value='contract' name='jobType' onClick={handleClick}>Contract</Button>
-        <Button value='temp' name='jobType' onClick={handleClick}>Temp Position</Button>
-        <Button value='seasonal' name='jobType' onClick={handleClick}>Seasonal</Button>
-        <Button value='internship' name='jobType' onClick={handleClick}>Internship</Button>
+        <Button value='Full Time' name='employment_type' onClick={handleClick}>Full Time</Button>
+        <Button value='partTime' name='employment_type' onClick={handleClick}>Part Time</Button>
+        <Button value='contract' name='employment_type' onClick={handleClick}>Contract</Button>
+        <Button value='temp' name='employment_type' onClick={handleClick}>Temp Position</Button>
+        <Button value='seasonal' name='employment_type' onClick={handleClick}>Seasonal</Button>
+        <Button value='internship' name='employment_type' onClick={handleClick}>Internship</Button>
       </ButtonGroup>
     </Label>
   )
@@ -201,30 +250,30 @@ const renderShiftSchedule = () => {
   return (
     <Label>Shift Schedule
       <ButtonGroup>
-        <Button value='4hourShift' name='shiftSchedule' onClick={handleClick}>4 hour shift</Button>
-        <Button value='8hourShift' name='shiftSchedule' onClick={handleClick}>8 hour shift</Button>
-        <Button value='10hourShift' name='shiftSchedule' onClick={handleClick}>10 hour shift</Button>
-        <Button value='12hourShift' name='shiftSchedule' onClick={handleClick}>12 hour shift</Button>
-        <Button value='3x12' name='shiftSchedule' onClick={handleClick}>3 x 12 hour shifts</Button>
-        <Button value='4x10' name='shiftSchedule' onClick={handleClick}>4 x 10 hour shifts</Button>
-        <Button value='4x12' name='shiftSchedule' onClick={handleClick}>4 x 12 hour shifts</Button>
-        <Button value='5x8' name='shiftSchedule' onClick={handleClick}>5 x 8 hour shifts</Button>
-        <Button value='mondayToFriday' name='shiftSchedule' onClick={handleClick}>Monday to Friday</Button>
-        <Button value='dayShift' name='shiftSchedule' onClick={handleClick}>Day Shift</Button>
-        <Button value='nightShift' name='shiftSchedule' onClick={handleClick}>Night Shift</Button>
-        <Button value='eveningShift' name='shiftSchedule' onClick={handleClick}>Evening Shift</Button>
-        <Button value='noNights' name='shiftSchedule' onClick={handleClick}>No Nights</Button>
-        <Button value='overNights' name='shiftSchedule' onClick={handleClick}>Overnights</Button>
-        <Button value='weekendNights' name='shiftSchedule' onClick={handleClick}>Weekend Nights</Button>
-        <Button value='weekendOnly' name='shiftSchedule' onClick={handleClick}>Weekends Only</Button>
-        <Button value='noWeekend' name='shiftSchedule' onClick={handleClick}>No Weekends</Button>
-        <Button value='onCall'name='shiftSchedule' onClick={handleClick}>On Call</Button>
-        <Button value='holiday' name='shiftSchedule' onClick={handleClick}>Holidays Only</Button>
-        <Button value='selfSchedule' name='shiftSchedule' onClick={handleClick}>Self-Determined Schedule</Button>
-        <Button value='afterSchool' name='shiftSchedule' onClick={handleClick}>After School</Button>
-        <Button value='overtime' name='shiftSchedule' onClick={handleClick}>Overtime</Button>
-        <Button value='other' name='shiftSchedule' onClick={handleClick}>Other</Button>
-        <Button value='' name='shiftSchedule' onClick={handleClick}>clear</Button>
+        <Button value='4hourShift' name='shift_schedule' onClick={handleClick}>4 hour shift</Button>
+        <Button value='8hourShift' name='shift_schedule' onClick={handleClick}>8 hour shift</Button>
+        <Button value='10hourShift' name='shift_schedule' onClick={handleClick}>10 hour shift</Button>
+        <Button value='12hourShift' name='shift_schedule' onClick={handleClick}>12 hour shift</Button>
+        <Button value='3x12' name='shift_schedule' onClick={handleClick}>3 x 12 hour shifts</Button>
+        <Button value='4x10' name='shift_schedule' onClick={handleClick}>4 x 10 hour shifts</Button>
+        <Button value='4x12' name='shift_schedule' onClick={handleClick}>4 x 12 hour shifts</Button>
+        <Button value='5x8' name='shift_schedule' onClick={handleClick}>5 x 8 hour shifts</Button>
+        <Button value='mondayToFriday' name='shift_schedule' onClick={handleClick}>Monday to Friday</Button>
+        <Button value='dayShift' name='shift_schedule' onClick={handleClick}>Day Shift</Button>
+        <Button value='nightShift' name='shift_schedule' onClick={handleClick}>Night Shift</Button>
+        <Button value='eveningShift' name='shift_schedule' onClick={handleClick}>Evening Shift</Button>
+        <Button value='noNights' name='shift_schedule' onClick={handleClick}>No Nights</Button>
+        <Button value='overNights' name='shift_schedule' onClick={handleClick}>Overnights</Button>
+        <Button value='weekendNights' name='shift_schedule' onClick={handleClick}>Weekend Nights</Button>
+        <Button value='weekendOnly' name='shift_schedule' onClick={handleClick}>Weekends Only</Button>
+        <Button value='noWeekend' name='shift_schedule' onClick={handleClick}>No Weekends</Button>
+        <Button value='onCall'name='shift_schedule' onClick={handleClick}>On Call</Button>
+        <Button value='holiday' name='shift_schedule' onClick={handleClick}>Holidays Only</Button>
+        <Button value='selfSchedule' name='shift_schedule' onClick={handleClick}>Self-Determined Schedule</Button>
+        <Button value='afterSchool' name='shift_schedule' onClick={handleClick}>After School</Button>
+        <Button value='overtime' name='shift_schedule' onClick={handleClick}>Overtime</Button>
+        <Button value='other' name='shift_schedule' onClick={handleClick}>Other</Button>
+        <Button value='' name='shift_schedule' onClick={handleClick}>clear</Button>
       </ButtonGroup>
     </Label>
   )
@@ -251,6 +300,7 @@ return (
       {renderSalary()}
       {renderPayRate()}
       {renderDescription()}
+      {renderKeywords()}
       {renderAvailablePositions()}
       {renderJobType()}
       {renderShiftSchedule()}
