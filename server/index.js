50833de3ab9d25132274671ec33441a57d76fc21
Jean Kim <jeankim119@gmail.com>
2022-08-16 11:26:12 -0500

    starting from scratch

--- server/index.js
+++ server/index.js
@@ -46,19 +46,19 @@ app.post("/uploadFile", upload.array("file"), async (req, res) => {
   }
 });
 
-// app.get("/downloadFile", async (req, res) => {
+app.get("/downloadFile", async (req, res) => {
 
-//   // var resume = "https://jafar-2022.s3.amazonaws.com/Clover.docx"
-//   var resume = "https://jafar-2022.s3.amazonaws.com/Jean+Kim+Resume++(1).pdf"
-//   var searchWords = ["lucky", "calm",  "bites"]
+  // var resume = "https://jafar-2022.s3.amazonaws.com/Clover.docx"
+  var resume = "https://jafar-2022.s3.amazonaws.com/Jean+Kim+Resume++(1).pdf"
+  var searchWords = ["lucky", "calm",  "bites"]
 
-//   try {
-//     const results = await parseResume(resume, searchWords)
-//     return res.json({ status: "success" });
-//   } catch (err) {
-//     console.log(err);
-//   }
+  try {
+    const results = await parseResume(resume, searchWords)
+    return res.json({ status: "success" });
+  } catch (err) {
+    console.log(err);
+  }
 
-// });
+});
 
 module.exports.app = app;
