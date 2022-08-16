c53a7b89483b41f2601adfff6c8079f35e3a9dfe
Xiaqing Xu <xuxiaqing2011@hotmail.com>
2022-08-15 23:30:31 -0500

    added post and put routes

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,82 @@
 const model = require('./model.js');
+const client = require('./db.js');
+
 
 module.exports = {
+  findUserType: async (req, res) => {
+    const { uuid } = req.query;
+    const data  = await model.findUserType(uuid);
+    res.send(data);
+  },
 
+  addSeeker: async (req, res) => {
+    const seeker = req.body;
+    try {
+      await model.addSeeker(seeker);
+      res.sendStatus(201);
+    } catch(e) {
+      console.log('eeeeee', e);
+    }
+  },
+
+  addRecruiter: async (req, res) => {
+    const recruiter = req.body;
+    try {
+      await model.addRecruiter(recruiter);
+      res.sendStatus(201);
+    } catch(e) {
+      console.log('eeeeee', e);
+    }
+  },
+
+  addAJob: async (req, res) => {
+    const jobPosting = req.body;
+    try {
+      await model.addAJob(jobPosting);
+      res.sendStatus(201);
+    } catch(e) {
+      console.log('eeeeee', e);
+    }
+  },
+
+  applyForAJob: async (req, res) => {
+    const { application } = req.body;
+    try {
+      await model.applyForAJob(application);
+      res.sendStatus(201);
+    } catch(e) {
+      console.log('eeeeee', e);
+    }
+  },
+
+  removeCandidate: async (req, res) => {
+    const { seeker_uuid } = req.body;
+    try {
+      await model.removeCandidate(seeker_uuid);
+      res.sendStatus(201);
+    } catch(e) {
+      console.log('eeeeee', e);
+    }
+  },
+
+  // Do we track we get the job so application_status set to True
+  closePosting: async (req, res) => {
+    const { listing_id } = req.body;
+    try {
+      await model.closePosting(listing_id);
+      res.sendStatus(201);
+    } catch(e) {
+      console.log('eeeeee', e);
+    }
+  },
+
+  verifySalary: async (req, res) => {
+    const userInfo = req.body;
+    try {
+      await model.verifySalary(userInfo);
+      res.sendStatus(201);
+    } catch(e) {
+      console.log('eeeeee', e);
+    }
+  }
 }
--- server/model.js
+++ server/model.js
@@ -1,6 +1,53 @@
 const client = require('./db.js');
 
 module.exports = {
+  findUserType: (uuid) => {
+    const queryString = `SELECT account_type FROM "Firebase" WHERE user_uuid = ${uuid}`;
+    return client.query(queryString);
+  },
 
+  addSeeker: (seeker) => {
+    const { user_uuid, first_name, last_name, coord_lat, coord_long, pref_industry, resume_url } = seeker;
+    const queryString = `INSERT INTO "Seekers" VALUES (${user_uuid}, ${first_name}, ${last_name}, ${coord_lat}, ${coord_long}, ${pref_industry}, ${resume_url})`;
+    return client.query(queryString);
+  },
 
+  addRecruiter: (recruiter) => {
+    const { user_uuid, first_name, last_name, company_name } = recruiter;
+    const queryString = `INSERT INTO "Recruiters" VALUES (${user_uuid}, ${first_name}, ${last_name}, ${company_name})`;
+    return client.query(queryString);
+  },
+
+  addAJob: (jobPosting) => {
+    const { recruiter_uuid, industry, coord_lat, coord_long, is_remote, title, salary_low, salary_high, pay_adjuster, desc, num_positions, employment_type, requested_keywords } = jobPosting; //expect body to have all these, status should default to true
+    const queryString = `INSERT INTO "Listings" (recruiter_uuid, industry, coord_lat, coord_long, is_remote, title, salary_low, salary_high, pay_adjuster, "desc", num_positions, employment_type, requested_keywords) VALUES (${recruiter_uuid}, ${industry}, ${coord_lat}, ${coord_long}, ${is_remote}, ${title}, ${salary_low}, ${salary_high}, ${pay_adjuster}, ${desc}, ${num_positions}, ${employment_type}, ${requested_keywords})`;
+    return client.query(queryString);
+  },
+
+  applyForAJob: (application) => {
+    // what is matched_keywords???????
+    const { seeker_uuid, listing_id, coverletter_url, matched_keywords } = application;
+
+    const queryString = `INSERT INTO "SubmittedApplications" (seeker_uuid, listing_id, coverletter_url, matched_keywords) VALUES (${seeker_uuid}, ${listing_id}, ${coverletter_url}, ${matched_keywords})`;
+
+    return client.query(queryString);
+  },
+
+  removeCandidate: (seeker_uuid) => {
+    const queryString =`UPDATE "SubmittedApplications" SET application_status = false WHERE seeker_uuid = ${ seeker_uuid }`;
+    return client.query(queryString);
+  },
+
+  // Do we track we get the job so application_status set to True
+  closePosting: (listing_id) => {
+    const queryString = `UPDATE "Listings" SET status = false
+    WHERE listing_id = ${ listing_id }; UPDATE "SubmittedApplications" SET status = false WHERE listing_id = ${ listing_id };`;
+    return client.query(queryString);
+  },
+
+  verifySalary: (userInfo) => {
+    const { seeker_uuid, listing_id, didReceivePromisedPay } = userInfo;
+    const queryString = `UPDATE "SubmittedApplications" SET didReceivePromisedPay = ${didReceivePromisedPay} and application_status = "selected" WHERE seeker_uuid = ${seeker_uuid} and listing_id = ${ listing_id }`;
+    return client.query(queryString);
+  }
 }
