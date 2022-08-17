ce1d143f36da5a41bdc82610fefa56c78c097738
Xiaqing Xu <xuxiaqing2011@hotmail.com>
2022-08-16 22:13:19 -0500

    Reorganized views

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,41 +1,51 @@
 import React, { useContext } from 'react';
 import { AllContext } from '../../index.jsx';
 
-const Profile = () => {
-  const { userType } = useContext(AllContext);
+const Profile = ({ userType }) => {
+  // const { userType } = useContext(AllContext);
 
   if (userType === 'seeker') {
     const { first_name, last_name, career_catogery, resumeURL, zip } = useContext(AllContext);
     return (
       <>
-        <label> First Name: </label>
-        <input type="text" value={first_name}></input>
-
-        <label> Last Name: </label>
-        <input type="text" value={last_name}></input>
-
-        <label> Career_catogery: </label>
-        <input type="text" value={career_catogery}></input>
-
-        <label> Resume: </label>
-        <input type="text" value={resumeURL}></input>
-
-        <label> Location: </label>
-        <input type="text" value={ }></input>
+        <div>
+          <label> First Name: </label>
+          <input type="text" value={first_name}></input>
+        </div>
+        <div>
+          <label> Last Name: </label>
+          <input type="text" value={last_name}></input>
+        </div>
+        <div>
+          <label> Career_catogery: </label>
+          <input type="text" value={career_catogery}></input>
+        </div>
+        <div>
+          <label> Resume: </label>
+          <input type="text" value={resumeURL}></input>
+        </div>
+        <div>
+          <label> Location: </label>
+          <input type="text" value=""></input>
+        </div>
       </>
     )
   } else if (userType === 'recruiter') {
     const { first_name, last_name, email, company } = useContext(AllContext);
     return (
       <>
-        <label> First Name: </label>
-        <input type="text" value={first_name}></input>
-
-        <label> Last Name: </label>
-        <input type="text" value={last_name}></input>
-
-        <label> Company: </label>
-        <input type="text" value={company}></input>
+        <div>
+          <label> First Name: </label>
+          <input type="text" value={first_name}></input>
+        </div>
+        <div>
+          <label> Last Name: </label>
+          <input type="text" value={last_name}></input>
+        </div>
+        <div>
+          <label> Company: </label>
+          <input type="text" value={company}></input>
+        </div>
       </>
     )
   }
