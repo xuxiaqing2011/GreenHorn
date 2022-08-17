e38ba265f3b8f4cc2689ce9f7de429411360c940
Xiaqing Xu <xuxiaqing2011@hotmail.com>
2022-08-17 14:18:58 -0500

    lowercase component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,8 +5,8 @@ import { AllContext } from "../index.jsx";
 import LoginForm from "../components/loginComponents/LoginForm.jsx";
 import { InterviewInviteModal } from "../Google_API/interviewInvite.jsx";
 import PostJob from "../components/PostJob/PostJob.jsx";
-import HeaderGallery from "../Components/Header/ImageGallery.jsx";
-import FilterFunctions from "../Components/Filters/FilterFunctions.jsx";
+import HeaderGallery from "../components/Header/ImageGallery.jsx";
+import FilterFunctions from "../components/Filters/FilterFunctions.jsx";
 import Feed from "../components/Feed/Feed.jsx";
 
 import { fileUpload } from "../components/fileHandlers.jsx";
