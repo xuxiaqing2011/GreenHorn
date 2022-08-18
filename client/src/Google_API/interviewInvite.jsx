3b781ab4c72ce0c6bec63f328a112155bc5e2af5
Nicholas Kempkes <nicholas.kempkes@gmail.com>
2022-08-18 11:52:48 -0500

    Stylized InterviewInvite Modal

--- client/src/Google_API/interviewInvite.jsx
+++ client/src/Google_API/interviewInvite.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useContext } from 'react';
 import styled from 'styled-components';
-import { Column, Form, modalBG, Label } from '../../public/stylesheets/styles.js'
+import { Column, Form, modalBG, Label, Input } from '../../public/stylesheets/styles.js'
 import { AllContext } from "../index.jsx";
 import sendInvite from "./calendar_event.jsx";
 
 // Material UI
 import FormLabel from "@mui/material/FormLabel";
-import TextField from "@mui/material/TextField";
 import Select from "@mui/material/Select";
 import Button from "@mui/material/Button";
 import FormControl from "@mui/material/FormControl";
@@ -41,23 +40,39 @@ export const InterviewInviteModal = () => {
       <Button variant='contained' onClick={() => handleShow()} >Schedule Interview</Button>
       <ModalDiv block={show ? 'block' : 'none'}>
         <ContentDiv>
-          <FormControl>
-            <FormLabel><b>Start Date</b></FormLabel>
-            <TextField name="startDate" type="date" onChange={(e) => handleChange(e.target)} />
-            <FormLabel><b>Start Time</b></FormLabel>
-            <TextField name="startTime" type="time" onChange={(e) => handleChange(e.target)} />
-            <FormLabel><b>End Date</b></FormLabel>
-            <TextField name="endDate" type="date" onChange={(e) => handleChange(e.target)} />
-            <FormLabel><b>End Time</b></FormLabel>
-            <TextField name="endTime" type="time" onChange={(e) => handleChange(e.target)} />
-            <FormLabel><b>Location</b></FormLabel>
-            <TextField name="location" type="text" placeholder="zoom.url OR 123 adress" onChange={(e) => handleChange(e.target)} />
-            <FormLabel><b>Description</b></FormLabel>
-            <TextField name="description" type="text" placeholder="Important information for applicant to know"
-              onChange={(e) => handleChange(e.target)} />
-          </FormControl>
-          <Button onClick={() => handleSubmit()}>Submit</Button>
-          <Button onClick={handleClose}> Close </Button>
+          <StyledForm>
+            <Child>
+              <FormLabel><b>Start Date</b></FormLabel>
+              <Input name="startDate" type="date" onChange={(e) => handleChange(e.target)} />
+            </Child>
+            <Child>
+              <FormLabel><b>Start Time</b></FormLabel>
+              <Input name="startTime" type="time" onChange={(e) => handleChange(e.target)} />
+            </Child>
+            <Child>
+              <FormLabel><b>End Date</b></FormLabel>
+              <Input name="endDate" type="date" onChange={(e) => handleChange(e.target)} />
+            </Child>
+            <Child>
+              <FormLabel><b>End Time</b></FormLabel>
+              <Input name="endTime" type="time" onChange={(e) => handleChange(e.target)} />
+            </Child>
+          </StyledForm>
+          <Styled_Text>
+            <Child_Text>
+              <FormLabel><b>Location</b></FormLabel>
+              <Input style={{ width: '75%' }} name="location" type="text" placeholder="zoom.url OR 123 Address" onChange={(e) => handleChange(e.target)} />
+            </Child_Text>
+            <Child_Text>
+              <FormLabel><b>Description</b></FormLabel>
+              <Input style={{ width: '70%' }} name="description" type="text" placeholder="Add Details"
+                onChange={(e) => handleChange(e.target)} />
+            </Child_Text>
+          </Styled_Text>
+          <Button_Container>
+            <Button onClick={() => handleSubmit()}>Submit</Button>
+            <Button onClick={handleClose}> Close </Button>
+          </Button_Container>
         </ContentDiv>
       </ModalDiv>
     </div>
@@ -73,17 +88,51 @@ const ModalDiv = styled.div`
   width: 100%;
   height: 100%;
   background: rgba(0, 0, 0, 0.6);
+  z-index: 10000;
 `;
 
 
 const ContentDiv = styled.div`
   position: fixed;
   background: white;
-  width: 50%;
+  width: 30%;
   height: auto;
   top: 50%;
   left: 50%;
   border-radius: 10px;
   padding: 2rem;
   transform: translate(-50%, -50%);
+`;
+
+const StyledForm = styled(FormControl)`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  text-align: center;
+  align-self: center;
+`;
+
+const Child = styled.div`
+  margin: auto;
+  display: inline-block;
+  flex-basis: 35%;
+`;
+
+const Child_Text = styled.div`
+  margin: auto;
+  display: inline-block;
+  flex-basis: 80%;
+`;
+
+const Styled_Text = styled.div`
+    display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  text-align: center;
+  align-self: center;
+`;
+
+const Button_Container = styled.div`
+  width: 100%;
+  text-align: center;
 `;
