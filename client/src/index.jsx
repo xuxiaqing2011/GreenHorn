a4c5a9697b213d8e4008211dd6e6e77e08fb4fdf
Shirley Nguyen <shirley.nguyen8@outlook.com>
2022-08-18 18:55:07 -0500

    Fixed merge conflicts

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,19 +1,19 @@
 import React, { useState, createContext, useEffect, useContext } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import Home from './pages/Home.js';
-import SignUp from './pages/SignUp.js';
-import Seeker from './pages/Seeker';
-import Profile from './components/Profile/Profile.js';
-import Feed from './components/Feed/Feed.jsx';
+import Home from "./pages/Home.js";
+import SignUp from "./pages/SignUp.js";
+import Seeker from "./pages/Seeker";
+import Profile from "./components/Profile/Profile.js";
+import Feed from "./components/Feed/Feed.jsx";
 
-import Recruiter from './pages/Recruiter';
-import PostJob from './components/PostJob/PostJob.jsx'
-import { GlobalStyle } from '../public/stylesheets/styles';
-import { AuthProvider } from './components/AuthContext.jsx';
-import userLocation from './Google_API/userLocation.jsx'
+import Recruiter from "./pages/Recruiter";
+import PostJob from "./components/PostJob/PostJob.jsx";
+import { GlobalStyle } from "../public/stylesheets/styles";
+import { AuthProvider } from "./components/AuthContext.jsx";
+import userLocation from "./Google_API/userLocation.jsx";
 
-import NavigationBar from './components/NavBar/NavigationBar.jsx';
+import NavigationBar from "./components/NavBar/NavigationBar.jsx";
 
 export const AllContext = createContext();
 // ismounted? Grab lat & long of user
@@ -44,91 +44,71 @@ const App = () => {
 
   return (
     <div>
-
-    <Router>
-      <GlobalStyle />
-      <AuthProvider>
-        <AllContext.Provider
-          value={{
-            location,
-            setLocation,
-            uuid,
-            setUuid,
-            email,
-            setEmail,
-            firstName,
-            setFirstName,
-            lastName,
-            setLastName,
-            accountType,
-            setAccountType,
-            preferredIndustry,
-            setPreferredIndustry,
-            zipCode,
-            setZipCode,
-            company,
-            setCompany,
-            coord_lat,
-            setCoord_lat,
-            coord_long,
-            setCoord_long,
-            resumeUrl,
-            setResumeUrl,
-            coverLetterUrl,
-            setCoverLetterUrl,
-            defaultJobs,
-            setDefaultJobs,
-            appliedJobs,
-            setAppliedJobs,
-          }}
-        >
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="signUp" element={<SignUp />} />
-            {/* <Route path="signUpGoogle" element={<SignUpGoogle />} /> */}
-<<<<<<< HEAD
-            <Route path="seeker" element={<Seeker />}>
-              <Route index element={<Feed view={{ view: "seeker" }} />} />
-              <Route path="profile" element={<Profile userType={"seeker"} />} />
-            </Route>
-            <Route path="recruiter" element={<Recruiter />}>
-              <Route
-                index
-                element={
-                  <Feed
-                    view={{ view: "recruiter" }}
-                    applicants={[1, 2, 3, 4, 5, 6]}
-                  />
-                }
-              />
-              <Route
-                path="profile"
-                element={<Profile userType={"recruiter"} />}
-              />
-=======
-            <Route path="seeker" element={<Seeker />} >
-              <Route index element={<Feed view={{view:'seeker'}}/>} />
-              <Route path="profile" element={<Profile />} />
-            </Route>
-            <Route path="recruiter" element={<Recruiter />} >
-              <Route index element={<Feed view={{view:'recruiter'}} applicants={[1, 2, 3, 4, 5, 6]} />} />
-              <Route path="profile" element={<Profile />} />
->>>>>>> dev
-              <Route path="postAJob" element={<PostJob />} />
-            </Route>
-          </Routes>
-        </AllContext.Provider>
-      </AuthProvider>
-    </Router>
-<<<<<<< HEAD
+      <Router>
+        <GlobalStyle />
+        <AuthProvider>
+          <AllContext.Provider
+            value={{
+              location,
+              setLocation,
+              uuid,
+              setUuid,
+              email,
+              setEmail,
+              firstName,
+              setFirstName,
+              lastName,
+              setLastName,
+              accountType,
+              setAccountType,
+              preferredIndustry,
+              setPreferredIndustry,
+              zipCode,
+              setZipCode,
+              company,
+              setCompany,
+              coord_lat,
+              setCoord_lat,
+              coord_long,
+              setCoord_long,
+              resumeUrl,
+              setResumeUrl,
+              coverLetterUrl,
+              setCoverLetterUrl,
+              defaultJobs,
+              setDefaultJobs,
+              appliedJobs,
+              setAppliedJobs,
+            }}
+          >
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="signUp" element={<SignUp />} />
+              {/* <Route path="signUpGoogle" element={<SignUpGoogle />} /> */}
+              <Route path="seeker" element={<Seeker />}>
+                <Route index element={<Feed view={{ view: "seeker" }} />} />
+                <Route path="profile" element={<Profile />} />
+              </Route>
+              <Route path="recruiter" element={<Recruiter />}>
+                <Route
+                  index
+                  element={
+                    <Feed
+                      view={{ view: "recruiter" }}
+                      applicants={[1, 2, 3, 4, 5, 6]}
+                    />
+                  }
+                />
+                <Route path="profile" element={<Profile />} />
+                <Route path="postAJob" element={<PostJob />} />
+              </Route>
+            </Routes>
+          </AllContext.Provider>
+        </AuthProvider>
+      </Router>
+    </div>
   );
 };
-=======
-    </div>
-  )
-}
-
->>>>>>> dev
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
