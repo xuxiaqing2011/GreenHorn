46a8e4a086e23b9f6d7017c637b0f97b98a44a4d
Nicholas Kempkes <nicholas.kempkes@gmail.com>
2022-08-18 15:24:22 -0500

    ...

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,6 +7,7 @@ import { InterviewInviteModal } from "../Google_API/interviewInvite.jsx";
 import PostJob from "../components/PostJob/PostJob.jsx";
 import HeaderGallery from "../components/Header/ImageGallery.jsx";
 import Feed from "../components/Feed/Feed.jsx";
+import FilterFunctions from "../components/Filters/FilterFunctions.jsx";
 import { fileUpload } from "../components/fileHandlers.jsx";
 
 const Home = () => {
@@ -14,6 +15,8 @@ const Home = () => {
     <>
       <h1> LinkedOut Navbar </h1>
       <HeaderGallery />
+      <FilterFunctions />
+      <LoginForm />
       <h1> Jobs (newest 20) </h1>
 
       <div>
