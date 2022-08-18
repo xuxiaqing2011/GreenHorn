6ee6766272f45e082f710f10bfc871b22811b3e4
Shirley Nguyen <shirley.nguyen8@outlook.com>
2022-08-18 17:00:58 -0500

    Refactor GET urls and changed view height of gallery

--- client/src/components/Header/ImageGallery.jsx
+++ client/src/components/Header/ImageGallery.jsx
@@ -17,7 +17,7 @@ export default function HeaderGallery() {
       sx={{ width: "100vw", height: "450" }}
       variant="quilted"
       cols={6}
-      rowHeight={250}
+      rowHeight={"17vh"}
     >
       {itemData.map((item) => (
         <ImageListItem
