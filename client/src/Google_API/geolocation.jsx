742b7c743ef2840b62d3d5f86c89e62dda203fa6
Andrew Wallace <amw.infospecialist@gmail.com>
2022-08-18 11:47:06 -0500

    Update geolocation.jsx

--- client/src/Google_API/geolocation.jsx
+++ client/src/Google_API/geolocation.jsx
@@ -5,7 +5,6 @@ import ReactDOM from "react-dom";
 // Async function that converts zipcode into latitude and longitude with Google API
 const geoConverter = function(zipcode) {
   let token = ''; // ANCHOR add Google API KEY
->>>>>>> 8dc130b92293f0f2591caf3d0e183eced76ed2bb
   let getGeo = `https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode}&key=${token}`;
   const geoConverter = axios.post(getGeo);
   return geoConverter.then(response => {
