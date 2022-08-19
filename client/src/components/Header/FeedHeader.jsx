484437fcb655f8ea1e08e8170c206f59b5e1a1cd
Shirley Nguyen <shirley.nguyen8@outlook.com>
2022-08-19 09:24:28 -0500

    Committing changes before merge

--- client/src/components/Header/FeedHeader.jsx
+++ client/src/components/Header/FeedHeader.jsx
@@ -15,19 +15,19 @@ export default function FeedHeader() {
 
 
 
-const FeedHeader = styled(Row)`
-  width: 100vw;
-  padding: 5px;
-  align-items: flex-end;
-  font-size: 14pt;
-  font-weight: bold;
-  justify-content: center;
-`;
+// const FeedHeader = styled(Row)`
+//   width: 100vw;
+//   padding: 5px;
+//   align-items: flex-end;
+//   font-size: 14pt;
+//   font-weight: bold;
+//   justify-content: center;
+// `;
 
-const HeaderDivider = styled(Row)`
-  width: 95vw;
-  height: 0.1em;
-  margin: 5px;
-  justify-content: center;
-  background: linear-gradient(50deg, rgba(119, 201, 212, 0.75), rgba(87, 188, 144, 0.75));
-`
+// const HeaderDivider = styled(Row)`
+//   width: 95vw;
+//   height: 0.1em;
+//   margin: 5px;
+//   justify-content: center;
+//   background: linear-gradient(50deg, rgba(119, 201, 212, 0.75), rgba(87, 188, 144, 0.75));
+// `
